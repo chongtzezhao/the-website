dc3179a4a157e27693d7a3398d105eaf401b6d68
chongtzezhao <47619363+chongtzezhao@users.noreply.github.com>
2024-12-20 01:17:29 +0800

    refactor: remove redundant database connection config

--- api/storage/connection.py
+++ api/storage/connection.py
@@ -4,14 +4,9 @@ from sqlalchemy import create_engine
 
 load_dotenv()
 
-# Fetch database credentials from environment variables
-# Determine environment and set the appropriate database URL
-if os.getenv('VERCEL_ENV') == 'production':  # This checks if on Vercel production
-    DATABASE_URL = os.getenv("DATABASE_URL")  # Vercel environment variable
-else:
-    DATABASE_USERNAME = os.getenv("DATABASE_USERNAME")
-    DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD")
-    DATABASE_NAME = os.getenv("DATABASE_NAME")
-    DATABASE_URL = os.getenv("DATABASE_URL").format(DATABASE_NAME = DATABASE_NAME, DATABASE_USERNAME = DATABASE_USERNAME, DATABASE_PASSWORD = DATABASE_PASSWORD)
+DATABASE_USERNAME = os.getenv("DATABASE_USERNAME")
+DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD")
+DATABASE_NAME = os.getenv("DATABASE_NAME")
+DATABASE_URL = os.getenv("DATABASE_URL").format(DATABASE_NAME = DATABASE_NAME, DATABASE_USERNAME = DATABASE_USERNAME, DATABASE_PASSWORD = DATABASE_PASSWORD)
 
 engine = create_engine(DATABASE_URL)
