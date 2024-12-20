b0587ad0a0cce3cca96f6a75dc4b922e56dee8fd
chongtzezhao <47619363+chongtzezhao@users.noreply.github.com>
2024-12-20 12:36:23 +0800

    test: rename next.config to js

--- next.config.js
+++ next.config.js
@@ -6,25 +6,25 @@ const nextConfig = {
         source: "/api/:path*",
         destination:
           process.env.NODE_ENV === "development"
-            ? "http://localhost:8000/api/:path*"
+            ? "http://127.0.0.1:8000/api/:path*"
             : "/api/",
       },
       {
         source: "/docs",
         destination:
           process.env.NODE_ENV === "development"
-            ? "http://localhost:8000/api/docs"
+            ? "http://127.0.0.1:8000/api/docs"
             : "/api/docs",
       },
       {
         source: "/openapi.json",
         destination:
           process.env.NODE_ENV === "development"
-            ? "http://localhost:8000/api/openapi.json"
+            ? "http://127.0.0.1:8000/api/openapi.json"
             : "/api/openapi.json",
       },
     ];
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
